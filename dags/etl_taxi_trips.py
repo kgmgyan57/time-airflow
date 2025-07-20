e817582e2de911759d1925f7aa056fc1ec0cdf9e
mgyankumar <gyan.kumar@zalora.com>
2025-07-21 01:14:59 +0800

    amend_etl_dag

--- dags/etl_taxi_trips.py
+++ dags/etl_taxi_trips.py
@@ -1,71 +1,55 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 from airflow.providers.google.cloud.sensors.gcs import GCSObjectsWithPrefixExistenceSensor
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 
-# Define variables
+# Variables
 project_id_value = 'dottime-bronze'
 dataset_name = 'time_landing'
 table_name = 'taxi_trips_raw'
 gcs_bucket_name = 'dottime-landing-raw'
-gcs_uri = f'gs://{gcs_bucket_name}/*'
-table_ref = f"{project_id_value}.{dataset_name}.{table_name}"
+gcs_uri_prefix = ''  # Adjust if files are in a subfolder
 
-# Default arguments for the DAG
 default_args = {
     "owner": "gyan",
     "description": "Raw Trips Pipeline Dag",
     "depend_on_past": False,
     "start_date": datetime(2025, 7, 19),
-    "email_on_failure": True,
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
 
-# Create the DAG
-dag = DAG(
-    'gcs_to_bigquery_parquet_load',
+with DAG(
+    dag_id='gcs_to_bigquery_parquet_load',
     default_args=default_args,
-    description='Load parquet files from GCS to BigQuery with monthly partitioning',
+    description='Load Parquet files from GCS to BigQuery with monthly partitioning',
     schedule_interval=None,
     catchup=False,
     tags=['raw_taxi_trips', 'etl'],
-)
+) as dag:
 
-# Task 1: Check if files exist in GCS bucket
-check_gcs_files = GCSObjectsWithPrefixExistenceSensor(
-    task_id='check_gcs_files_exist',
-    bucket=gcs_bucket_name,
-    prefix='',
-    google_cloud_conn_id='google_cloud_default',
-    timeout=300,
-    poke_interval=60,
-    dag=dag,
-)
+    check_gcs_files_exist = GCSObjectsWithPrefixExistenceSensor(
+        task_id='check_gcs_files_exist',
+        bucket=gcs_bucket_name,
+        prefix=gcs_uri_prefix,
+        gcp_conn_id='google_cloud_default',
+        timeout=300,
+        poke_interval=60,
+    )
 
-# Task 2: Load data from GCS parquet files to BigQuery with partitioning
-load_parquet_to_bq = BigQueryInsertJobOperator(
-    task_id='load_parquet_to_bigquery',
-    configuration={
-        "load": {
-            "sourceUris": [gcs_uri],
-            "destinationTable": {
-                "projectId": project_id_value,
-                "datasetId": dataset_name,
-                "tableId": table_name,
-            },
-            "sourceFormat": "PARQUET",
-            "writeDisposition": "WRITE_APPEND",
-            "autodetect": True,
-            "timePartitioning": {
-                "type": "MONTH",
-                "field": "time_start_timestamp",
-            },
-        }
-    },
-    google_cloud_conn_id='google_cloud_default',
-    dag=dag,
-)
+    load_parquet_to_bigquery = GCSToBigQueryOperator(
+        task_id='load_parquet_to_bigquery',
+        bucket=gcs_bucket_name,
+        source_objects=['*'],
+        destination_project_dataset_table=f'{project_id_value}.{dataset_name}.{table_name}',
+        source_format='PARQUET',
+        write_disposition='WRITE_APPEND',
+        autodetect=True,
+        time_partitioning={
+            "type": "MONTH",
+            "field": "time_start_timestamp"
+        },
+        gcp_conn_id='google_cloud_default',
+    )
 
-# Define task dependencies
-check_gcs_files >> load_parquet_to_bq
+    check_gcs_files_exist >> load_parquet_to_bigquery
